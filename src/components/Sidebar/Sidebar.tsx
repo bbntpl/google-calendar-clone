4f08f8a2f30165953fabd2012eb908e9b4a9d820
bvrbryn445 <beaverbryanantipolo@gmail.com>
2022-08-15 20:55:51 -0400

    feat: Sidebar component

---
+++ src/components/Sidebar/Sidebar.tsx
@@ -0,0 +1,40 @@
+import './styles.scss';
+import SortDown from '../../assets/icons/sort-down.png';
+
+import MiniCalendar from '../MiniCalendar';
+import { GooglePlus } from '../../lib/icons';
+import CalendarList from '../CalendarList';
+import withScheduleModalToggle, { ScheduleCreationProps } from '../Schedules/components/ScheduleHOC';
+
+function ScheduleBtn(props: ScheduleCreationProps): JSX.Element {
+	const { hocMethods } = props;
+	const { toggleVisibility } = hocMethods;
+	return (
+		<div className='create-schedule-wrapper'>
+			<div className='create-schedule-inner'>
+				<button
+					className='create-schedule row center-xs middle-xs'
+					onClick={toggleVisibility}
+				>
+					<GooglePlus />
+					Create
+					<img className='icon--small' src={SortDown} />
+				</button>
+			</div>
+		</div>
+	)
+}
+
+const CreateSchedule = withScheduleModalToggle(ScheduleBtn);
+
+export default function Sidebar(): JSX.Element {
+	return (
+		<div className='sidebar'>
+			<div>
+				<CreateSchedule />
+			</div>
+			<MiniCalendar rootElModifier={'by-content'} />
+			<CalendarList />
+		</div>
+	)
+}
